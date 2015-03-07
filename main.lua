39d700734a228535450f053cc9ddb0612fc14543
Matthew Pfeiffer <spferical@gmail.com>
2015-03-07 12:39:19 -0500

    Move variables to player object

--- main.lua
+++ main.lua
@@ -4,16 +4,16 @@ debug = true
 player = {
     x = 20,
     y = 20,
+    image = nil,
+    height = 32,
+    width = 32,
 }
 
-player_image = nil
-player_height = 32
-player_width = 32
 random = love.math.newRandomGenerator()
 
 
 function love.load(arg)
-    player_image = love.graphics.newImage("assets/player.png")
+    player.image = love.graphics.newImage("assets/player.png")
 end
 
 function love.update(dt)
@@ -47,12 +47,12 @@ end
 function check_player_collision()
     x1 = math.floor(player.x / tile_w)
     y1 = math.floor(player.y / tile_h)
-    x2 = math.floor((player.x + player_width) / tile_w)
-    y2 = math.floor((player.y + player_height) / tile_h)
+    x2 = math.floor((player.x + player.width) / tile_w)
+    y2 = math.floor((player.y + player.height) / tile_h)
     return map_grid[x1][y1] == tiles.wall or map_grid[x2][y2] == tiles.wall
 end
 
 function love.draw()
     draw_map()
-    love.graphics.draw(player_image, player.x, player.y)
+    love.graphics.draw(player.image, player.x, player.y)
 end
