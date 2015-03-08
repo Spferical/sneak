efe766519afa48237b53453547724d8d51ba23d1
Matthew Pfeiffer <spferical@gmail.com>
2015-03-08 17:48:13 -0400

    Basic guard FOV
    Still needs exact tile intersections

--- main.lua
+++ main.lua
@@ -147,6 +147,13 @@ end
 function love.draw()
     camera:attach()
     draw_map(camera)
+    love.graphics.setColor(100, 100, 100, 100)
+    for i, guard in ipairs(guards) do
+        for j, t in ipairs(get_fov(guard.x, guard.y, 500)) do
+            love.graphics.polygon('fill', t)
+        end
+    end
+    love.graphics.setColor(255, 255, 255, 255)
     love.graphics.draw(player.image, player.x , player.y)
     draw_guards()
     camera:detach()
--- map.lua
+++ map.lua
@@ -23,6 +23,108 @@ tile_w = 48
 tile_h = 48
 
 
+function line_intersects_wall(x1, y1, x2, y2)
+    -- returns the wall intersection closest to (x1, y1) if one exists on the
+    -- line. else, returns {x2, y2}
+    points = grid_line_intersection(x1 / tile_w, y1 / tile_h,
+        x2 / tile_w, y2 / tile_h)
+    for i, p in ipairs(points) do
+        map_x = math.floor(p.x)
+        map_y = math.floor(p.y)
+        if map.grid[map_x][map_y] == tiles.wall then
+            -- return intersection point
+            return (map_x + .5) * tile_w, (map_y + .5) * tile_h
+        end
+    end
+    return x2, y2
+end
+
+function grid_line_intersection(x1, y1, x2, y2)
+    -- returns the map points that are intersected by a given line
+    -- via https://github.com/Yonaba/Algorithm-Implementations/blob/master/Bresenham_Based_Supercover_Line/Lua/Yonaba/bresenham_based_supercover.lua
+    local points = {}
+    local xstep, ystep, err, errprev, ddx, ddy
+    local x, y = x1, y1
+    local dx, dy = x2 - x1, y2 - y1
+    points[#points + 1] = {x = x1, y = y1}
+    if dy < 0 then
+        ystep = - 1
+        dy = -dy
+    else
+        ystep = 1
+    end
+    if dx < 0 then
+        xstep = - 1
+        dx = -dx
+    else
+        xstep = 1
+    end
+    ddx, ddy = dx * 2, dy * 2
+    if ddx >= ddy then
+        errprev, err = dx, dx
+        for i = 1, dx do
+            x = x + xstep
+            err = err + ddy
+            if err > ddx then
+                y = y + ystep
+                err = err - ddx
+                if err + errprev < ddx then
+                    points[#points + 1] = {x = x, y = y - ystep}
+                elseif err + errprev > ddx then
+                    points[#points + 1] = {x = x - xstep, y = y}
+                else
+                    points[#points + 1] = {x = x, y = y - ystep}
+                    points[#points + 1] = {x = x - xstep, y = y}
+                end
+            end
+            points[#points + 1] = {x = x, y = y}
+            errprev = err
+        end
+    else
+        errprev, err = dy, dy
+        for i = 1, dy do
+            y = y + ystep
+            err = err + ddx
+            if err > ddy then
+                x = x + xstep
+                err = err - ddy
+                if err + errprev < ddy then
+                    points[#points + 1] = {x = x - xstep, y = y}
+                elseif err + errprev > ddy then
+                    points[#points + 1] = {x = x, y = y - ystep}
+                else
+                    points[#points + 1] = {x = x, y = y - ystep}
+                    points[#points + 1] = {x = x - xstep, y = y}
+                end
+            end
+            points[#points + 1] = {x = x, y = y}
+            errprev = err
+        end
+    end
+    return points
+end
+
+function pixel_to_map_coords(x, y)
+    return math.floor(x / tile_w), math.floor(y / tile_h)
+end
+
+function get_fov(x, y, dist)
+    -- algorithm described at http://www.redblobgames.com/articles/visibility/
+    triangles = {}
+    step = math.pi / 50
+    for i = 0, 2 * math.pi - step, step do
+        x1 = x + math.cos(i) * dist
+        y1 = y + math.sin(i) * dist
+        x2 = x + math.cos(i + step) * dist
+        y2 = y + math.sin(i + step) * dist
+        x1, y1 = line_intersects_wall(x, y, x1, y1)
+        x2, y2 = line_intersects_wall(x, y, x2, y2)
+        table.insert(triangles, {x, y, x1, y1, x2, y2})
+    end
+    return triangles
+end
+
+
 function generate_map()
     map.grid = {}
     -- fill the map with floor tiles
