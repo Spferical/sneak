1349bdffbc5f992b9f3725e51b382c31f19c64bd
Matthew Pfeiffer <spferical@gmail.com>
2015-03-10 18:45:04 -0400

    Work on enhancing FOV. Optimized a bunch.

--- map.lua
+++ map.lua
@@ -26,14 +26,42 @@ tile_h = 48
 function line_intersects_wall(x1, y1, x2, y2)
     -- returns the wall intersection closest to (x1, y1) if one exists on the
     -- line. else, returns {x2, y2}
-    points = grid_line_intersection(x1 / tile_w, y1 / tile_h,
+    local points = grid_line_intersection(x1 / tile_w, y1 / tile_h,
         x2 / tile_w, y2 / tile_h)
     for i, p in ipairs(points) do
-        map_x = math.floor(p.x)
-        map_y = math.floor(p.y)
-        if map.grid[map_x][map_y] == tiles.wall then
+        local map_x = math.floor(p.x)
+        local map_y = math.floor(p.y)
+        if map_x >= 0 and map_x < map.width and map_y >= 0 and map_y < map.height and map.grid[map_x][map_y] == tiles.wall then
             -- return intersection point
-            return (map_x + .5) * tile_w, (map_y + .5) * tile_h
+            local mx = map_x * tile_w
+            local my = map_y * tile_h
+            local left = mx - 1
+            local right = mx + tile_w + 1
+            local top = my - 1
+            local bottom = my + tile_h + 1
+            if x1 <= mx then
+                local x, y = findIntersect(x1, y1, x2, y2, mx, my, mx, my + tile_h, true, true)
+                if x ~= false then
+                    return x, y
+                end
+            elseif x1 >= mx then
+                local x, y = findIntersect(x1, y1, x2, y2, mx + tile_w, my, mx + tile_w, my + tile_h, true, true)
+                if x ~= false then
+                    return x, y
+                end
+            end
+            if y1 <= my then
+                local x, y = findIntersect(x1, y1, x2, y2, mx, my, mx + tile_w, my, true, true)
+                if x ~= false then
+                    return x, y
+                end
+            elseif y1 >= my then
+                local x, y = findIntersect(x1, y1, x2, y2, mx, my + tile_h, mx + tile_w, my + tile_h, true, true)
+                if x ~= false then
+                    return x, y
+                end
+            end
+            print('ugh', x1, y1, x2, y2, map_x, map_y, x, y)
         end
     end
     return x2, y2
@@ -104,6 +132,25 @@ function grid_line_intersection(x1, y1, x2, y2)
     return points
 end
 
+-- via https://love2d.org/wiki/General_math
+-- Checks if two lines intersect (or line segments if seg is true)
+-- Lines are given as four numbers (two coordinates)
+function findIntersect(l1p1x,l1p1y, l1p2x,l1p2y, l2p1x,l2p1y, l2p2x,l2p2y, seg1, seg2)
+    local a1,b1,a2,b2 = l1p2y-l1p1y, l1p1x-l1p2x, l2p2y-l2p1y, l2p1x-l2p2x
+    local c1,c2 = a1*l1p1x+b1*l1p1y, a2*l2p1x+b2*l2p1y
+    local det,x,y = a1*b2 - a2*b1
+    if det==0 then return false, "The lines are parallel." end
+    x,y = (b2*c1-b1*c2)/det, (a1*c2-a2*c1)/det
+    if seg1 or seg2 then
+        local min,max = math.min, math.max
+        if seg1 and not (min(l1p1x,l1p2x) <= x and x <= max(l1p1x,l1p2x) and min(l1p1y,l1p2y) <= y and y <= max(l1p1y,l1p2y)) or
+           seg2 and not (min(l2p1x,l2p2x) <= x and x <= max(l2p1x,l2p2x) and min(l2p1y,l2p2y) <= y and y <= max(l2p1y,l2p2y)) then
+            return false, "The lines don't intersect."
+        end
+    end
+    return x,y
+end
+
 function pixel_to_map_coords(x, y)
     return math.floor(x / tile_w), math.floor(y / tile_h)
 end
@@ -115,17 +162,18 @@ function point_is_in_wall(x, y)
 end
 
 function get_fov(x, y, dist)
-    -- algorithm described at http://www.redblobgames.com/articles/visibility/
-    triangles = {}
-    step = math.pi / 50
+    -- ideal algorithm described at http://www.redblobgames.com/articles/visibility/
+    local triangles = {}
+    local step = math.pi / 32
+    local i = 0
+    local last_x, last_y = line_intersects_wall(x, y, dist, 0)
     for i = 0, 2 * math.pi - step, step do
-        x1 = x + math.cos(i) * dist
-        y1 = y + math.sin(i) * dist
-        x2 = x + math.cos(i + step) * dist
-        y2 = y + math.sin(i + step) * dist
+        local x1 = x + math.cos(i) * dist
+        local y1 = y + math.sin(i) * dist
         x1, y1 = line_intersects_wall(x, y, x1, y1)
-        x2, y2 = line_intersects_wall(x, y, x2, y2)
-        table.insert(triangles, {x, y, x1, y1, x2, y2})
+        table.insert(triangles, {x, y, last_x, last_y, x1, y1})
+        last_x = x1
+        last_y = y1
     end
     return triangles
 end
